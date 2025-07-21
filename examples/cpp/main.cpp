aa4fb197bd9245ab16c64971cd338e092f27f75c
Viacheslav <viacheslav@mcublog.ru>
2025-07-21 18:49:37 +0300

    bump version; upd cpp example;

--- examples/cpp/main.cpp
+++ examples/cpp/main.cpp
@@ -18,15 +18,18 @@ int main()
     uint8_t bitfield_array[2] = {0};
     bitarray bits {bitfield_array, sizeof(bitfield_array) * 8};
 
-    bits.set(0);
-    bits.set(15);
+    bits[0] = 1;
+    bits[15] = 1;
+
+    uint16_t by_bits = bits[15] << 15 | bits[0];
 
     uint16_t *value = (uint16_t *)bitfield_array;
 
-    printf("15 and 0 bits == 0x%0x\r\n", *value);
+    printf("15 and 0 bits == 0x%0x == 0x%0x\r\n", *value, by_bits);
 
     // b1000_0000_0000_0001 -- 15 bit and 0 bit
-    assert((*value) == 0x8001);
+    assert((*value) == by_bits);
+    assert(by_bits == 0x8001);
 
     return 0;
 }
